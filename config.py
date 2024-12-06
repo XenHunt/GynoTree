ff008582a1c85831f292b437d9b61891d5add5e0
XenHunt <95288387+XenHunt@users.noreply.github.com>
2024-12-06 12:52:21 +0300

    Changes: 1. Added "NAME" of DB. 2. Added configuration for postgresql instances.

--- config.py
+++ config.py
@@ -13,6 +13,7 @@ configuration = {
     "DEBUG": _get_env_variable("DEBUG", bool, False),
     "DB": {
         "HOST": _get_env_variable("DB_HOST", str, "localhost"),
+        "NAME": _get_env_variable("DB_NAME", str, "db"),
         "USER": _get_env_variable("DB_USER", str, "root"),
         "PORT": _get_env_variable("DB_PORT", int, 3306),
         "PASSWORD": _get_env_variable("DB_PASSWORD", str, ""),
--- main.py
+++ main.py
@@ -3,5 +3,8 @@ from .config import configuration
 
 app = Flask("Family Tree")
 
+app.config["SQLALCHEMY_DATABASE_URI"] = (
+    f"postgresql://{configuration['DB']['USER']}:{configuration['DB']['PASSWORD']}@{configuration['DB']['HOST']}:{configuration['DB']['PORT']}/{configuration['DB']['NAME']}"
+)
 
 app.run(debug=configuration["DEBUG"])
